5cad9d594abac0b4d6e63b9e469d5324c23e86d4
Md Wahiduzzaman Bappy <141256969+Bappy-se-0207@users.noreply.github.com>
2023-08-02 21:58:20 +0600

    Add files via upload

---
+++ assignment01.dart
@@ -0,0 +1,15 @@
+void main(){
+  int a = 11;
+  int b = 5;
+  int resultaddition= a + b;
+  int resultsubtraction= a - b;
+  int resultmultiplication= a * b;
+  double resultdivision= a / b;
+  int resultmodulus= a % b;
+  print(resultaddition);
+  print(resultsubtraction);
+  print(resultmultiplication);
+  print(resultdivision);
+  print(resultmodulus);
+
+}
